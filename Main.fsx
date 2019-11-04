241d2cc4a0c4ebf70d1b7473e78f1fc638e03160
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2019-11-04 21:55:06 +0100

    autoformat

--- Main.fsx
+++ Main.fsx
@@ -1,46 +1,43 @@
-let joinWith separator (iterable: seq<string>) =
-  System.String.Join(separator, iterable)
+let joinWith separator (iterable: seq<string>) = System.String.Join(separator, iterable)
 
 module Task =
-  type Task = {
-    rj: int;
-    pj: int;
-    dj: int;
-  }
+    type Task =
+        { rj: int
+          pj: int
+          dj: int }
 
-  let toString (task: Task): string =
-    [task.rj; task.pj; task.dj]
-      |> List.map string
-      |> joinWith " "
+    let toString (task: Task): string =
+        [ task.rj; task.pj; task.dj ]
+        |> List.map string
+        |> joinWith " "
 
-  let generateRandom (generator: System.Random) =
-    {
-      rj = (generator.Next(1, 10));
-      pj = (generator.Next(1, 10));
-      dj = (generator.Next(1, 10));
-    }
+    let generateRandom (generator: System.Random) =
+        { rj = (generator.Next(1, 10))
+          pj = (generator.Next(1, 10))
+          dj = (generator.Next(1, 10)) }
 
 module Instance =
-  type Instance = {
-    size: int;
-    tasks: seq<Task.Task>;
-  }
+    type Instance =
+        { size: int
+          tasks: seq<Task.Task> }
 
-  let generate randomGenerator size =
-    let tasks =
-      seq { 1 .. size }
-      |> Seq.map (fun _ -> Task.generateRandom randomGenerator)
+    let generate randomGenerator size =
+        let tasks = seq { 1 .. size } |> Seq.map (fun _ -> Task.generateRandom randomGenerator)
 
-    {
-      size = size;
-      tasks = tasks;
-    }
+        { size = size
+          tasks = tasks }
 
-  let toString (i: Instance) =
-    let sizeString = string i.size
-    let taskStrings = i.tasks |> Seq.map Task.toString |> List.ofSeq
 
-    sizeString :: taskStrings |> joinWith "\n"
+    let toString (i: Instance) =
+        let sizeString = string i.size
+
+        let taskStrings =
+            i.tasks
+            |> Seq.map Task.toString
+            |> List.ofSeq
+
+        sizeString :: taskStrings
+        |> joinWith "\n"
 
 
 // MAIN
@@ -49,7 +46,5 @@ let n = 50
 let randomGenerator = System.Random()
 
 Instance.generate randomGenerator n
-  |> Instance.toString
-  |> printf "%A"
-
-
+|> Instance.toString
+|> printf "%A"

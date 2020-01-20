6db24847c388c07564f17bd9ca59c2d093c6f011
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2020-01-20 11:50:26 +0100

    wip monte carlo search

--- Main.fsx
+++ Main.fsx
@@ -45,16 +45,13 @@ module ListAlgorithm =
     let tasks (m: Machine) =
       m.tasks
 
+  let machinesToSolution (machines: Machine list): Solution =
+     machines
+     |> List.sortBy Machine.id
+     |> List.map Machine.tasks
+     |> nestedListsToSeqs
 
-  let solveFirstFreeMachineByReadyTime (instance: Instance): Solution =
-    let machinesToSolution (machines: Machine list): Solution =
-       machines
-       |> List.sortBy Machine.id
-       |> List.map Machine.tasks
-       |> nestedListsToSeqs
-
-    let sortedTasks = Seq.sortBy Task.readyTime instance |> List.ofSeq
-
+  let assignToFirstFreeMachine (tasks: Task list): Solution =
     let firstReadyMachine machines = List.minBy Machine.lastEnd machines
 
     let rec step machines tasksLeft =
@@ -62,18 +59,79 @@ module ListAlgorithm =
       | [] -> machines
       | task::rest ->
           let selectedMachine = firstReadyMachine machines
-          let withTask = Machine.addTask task selectedMachine
-          let newMachines = replaceInList selectedMachine withTask machines
+          let machineWithTask = Machine.addTask task selectedMachine
+          let newMachines = replaceInList selectedMachine machineWithTask machines
 
           step newMachines rest
 
     let initialMachines = [(Machine.empty 1); (Machine.empty 2); (Machine.empty 3); (Machine.empty 4)]
 
-    step initialMachines sortedTasks |> machinesToSolution
+    step initialMachines tasks |> machinesToSolution
+
+  let solveFirstFreeMachineByReadyTime (instance: Instance): Solution =
+    let sortedTasks = Seq.sortBy Task.readyTime instance |> List.ofSeq
+    assignToFirstFreeMachine sortedTasks
+
+
+module AdvancedAlgoritm =
+  let swap (a: _[]) x y =
+      let tmp = a.[x]
+      a.[x] <- a.[y]
+      a.[y] <- tmp
+
+  // shuffle an array (in-place)
+  let shuffle (rand: Random) a =
+      Array.iteri (fun i _ -> swap a i (rand.Next(i, Array.length a))) a
+
+  let shuffleList (rand: Random) l =
+      let mutable xs = Array.ofList l
+      shuffle rand xs
+      List.ofArray xs
+
+  let permutations (random: Random) (k: int) (l: 'a list): seq<'a list> =
+    seq { 1..k } |> Seq.map (fun _ -> shuffleList random l)
+
+  let removeTask (x: Task) (l: Task list): Task list =
+    List.filter (fun t -> t <> x) l
+
+  let mean (xs: seq<int>): float =
+    (Seq.sum xs) ./. (Seq.length xs)
+
+  let solveMonteCarlo (instance: Instance): Solution =
+    let k = 2
+    let aggr = mean
+
+    let tasks = List.ofSeq instance
+    let random = Random()
+
+    let calculateCriteria (path: Task list): int =
+      ListAlgorithm.assignToFirstFreeMachine path |> Solution.totalLateness
+
+    let calculateNodeValue fixedNodes nodesLeft (task: Task): float =
+      let permutations = permutations random k (removeTask task nodesLeft)
+      let fullPaths = Seq.map (List.append fixedNodes) permutations
+      let criteria = Seq.map calculateCriteria fullPaths
+      aggr criteria
+
+    let rec step (fixedNodes: Task list) (nodesLeft: Task list): Task list =
+      if (List.length nodesLeft) = 0
+      then
+        fixedNodes
+      else
+        printfn "%A" fixedNodes
+        let maxValueNode = List.minBy (calculateNodeValue fixedNodes nodesLeft) nodesLeft
+
+        let nextFixedNodes = List.append fixedNodes [maxValueNode]
+        let nextNodesLeft = removeTask maxValueNode nodesLeft
+
+        step nextFixedNodes nextNodesLeft
+
+    let tasks = step [] tasks
+
+    ListAlgorithm.assignToFirstFreeMachine tasks
 
 
 
-// MAIN
 let indexNumber = 133865
 
 let generateInstanceAndSolution n =
@@ -178,4 +236,14 @@ let generateMeasurementsForAllInDirectory dir =
   |> Array.groupBy (fun (path, _result) -> studentIdFromPath path)
   |> Array.iter printResults
 
-solveAllInDirectory solveReference "wszystkie-wrzucone"
+
+// MAIN
+
+let instance = Instance.fromFile "moje-wrzucone/in133865_500.txt"
+
+let result = instance
+              |> AdvancedAlgoritm.solveMonteCarlo
+              |> Solution.totalLateness
+
+printfn "%A" result
+
